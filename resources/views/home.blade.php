da3e6f292dcf991414c3c9b136f96d6d53b7e8db
christian-sosa <sosa.christian.agustin@gmail.com>
2019-12-21 12:16:50 -0300

    Finalizado

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,16 +1,251 @@
 <!DOCTYPE html>
-<html lang="en" dir="ltr">
-  <head>
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-    <meta charset="utf-8">
-    <title></title>
-  </head>
-  <body>
+<html lang="en">
+<head>
+	<title>Christian Agustin Sosa - CV</title>
+	<meta charset="UTF-8">
+	<meta name="description" content="Civic - CV Resume">
+	<meta name="keywords" content="resume, civic, onepage, creative, html">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<!-- Favicon -->
+	<link href="img/favicon.ico" rel="shortcut icon"/>
 
-    <div class="text-center">
-  <img src="..." class="rounded" alt="...">
-</div>
+	<!-- Google Fonts -->
+	<link href="https://fonts.googleapis.com/css?family=Josefin+Sans:400,400i,600,600i,700" rel="stylesheet">
 
-  </body>
+	<!-- Stylesheets -->
+	<link rel="stylesheet" href="/css/fontawesome/css/all.css">
+	<link rel="stylesheet" href="css/bootstrap.min.css"/>
+	<link rel="stylesheet" href="css/font-awesome.min.css"/>
+	<link rel="stylesheet" href="css/flaticon.css"/>
+	<link rel="stylesheet" href="css/owl.carousel.css"/>
+	<link rel="stylesheet" href="css/magnific-popup.css"/>
+	<link rel="stylesheet" href="css/style.css"/>
+
+
+	<!--[if lt IE 9]>
+	<script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+	<script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+	<![endif]-->
+
+</head>
+<body>
+	<!-- Page Preloder -->
+	<div id="preloder">
+		<div class="loader"></div>
+	</div>
+
+	<!-- Header section start -->
+	<header class="header-section">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-md-4">
+					<div class="site-logo">
+						<h2><a href="#"></a></h2>
+						<p></p>
+					</div>
+				</div>
+				<div class="col-md-8 text-md-right header-buttons">
+					<a href="/storage/cvchristiansosa.pdf" download="christian sosa cv" class="site-btn">Descargar CV</a>
+					<a href="https://www.linkedin.com/in/sosac/" class="site-btn">Mas sobre mi</a>
+				</div>
+			</div>
+		</div>
+	</header>
+	<!-- Header section end -->
+
+	<!-- Hero section start -->
+	<section class="hero-section spad">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-xl-10 offset-xl-1">
+					<div class="row">
+						<div class="col-lg-6">
+							<div class="hero-text">
+								<h2>Christian Sosa</h2>
+								<p>Desarrollador web full stack</p>
+							</div>
+							<div class="hero-info">
+								<h2>Informacion general</h2>
+								<ul>
+									<li><span>Fecha de nacimiento</span>   12/07/1995</li>
+									<li><span>Direccion</span>   Lamadrid 5134, Lomas de zamora</li>
+									<li><span>E-mail</span>   sosa.christian.agustin@gmail.com</li>
+									<li><span>Telefono </span>   1530115309</li>
+								</ul>
+							</div>
+						</div>
+						<div class="col-lg-6">
+							<figure class="hero-image">
+								<img src="/storage/qweqwe.jpg" alt="5">
+							</figure>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Hero section end -->
+
+	<!-- Social links section start -->
+	<div class="social-section">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-xl-10 offset-xl-1">
+					<div class="social-link-warp">
+						<div class="social-links">
+							<a href="https://www.linkedin.com/in/sosac/"><i class="fab fa-linkedin"></i></i></a>
+							<a href="https://github.com/christian-sosa"><i class="fab fa-github"></i></i></a>
+						</div>
+						<h2 class="hidden-md hidden-sm">Links</h2>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<!-- Social links section end -->
+
+	<!-- Resume section start -->
+	<section class="resume-section spad">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-xl-7 offset-xl-2">
+					<div class="section-title">
+						<h2>Experiencia Profesional</h2>
+					</div>
+					<ul class="resume-list">
+						<li>
+							<h2>Agosto 2019-Diciembre 2019</h2>
+							<h3>Desarrollo ecommerce</h3>
+							<h4>Desarrollador web full stack trainee</h4>
+							<p>Ecommerce utilizando HTML para la estructura, CSS para el diseño y Javascript para interacciones y/o validaciones del lado del cliente. El diseño y desarrollo del FrontEnd toma las buenas practicas de hacer sitios responsive El Backend y la API REST fue desarrollado en PHP, con Laravel como framework de apoyo utilizando buenas practicas MVC Diseñe y utilice un UML para el armado y organización de la base de datos relacional con la que luego interactuamos a través de MySQL </p>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</section>
+	<section class="resume-section spad">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-xl-7 offset-xl-2">
+					<div class="section-title">
+						<h2>Experiencia laboral</h2>
+					</div>
+					<ul class="resume-list">
+						<li>
+							<h2>2017-2018</h2>
+							<h3>Gestion de procesos y servicios</h3>
+							<h4>Help Desk</h4>
+							<p>Soporte tecnico a clientes de Telecentro via CRM </p>
+						</li>
+						<li>
+							<h2>2015-2017</h2>
+							<h3>Burger King</h3>
+							<h4>Crew</h4>
+							<p>Atencion al cliente, ventas </p>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Resume section end -->
+
+	<!-- Resume section start -->
+	<section class="resume-section with-bg spad">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-xl-7 offset-xl-2">
+					<div class="section-title">
+						<h2>Educacion</h2>
+					</div>
+					<ul class="resume-list">
+						<li>
+							<h2>2019</h2>
+							<h3>Digital House</h3>
+							<h4>Programacion web full stack</h4>
+						</li>
+						<li>
+							<h2>2017-En curso</h2>
+							<h3>Universidad Nacional de Lanus</h3>
+							<h4>Licenciatura en sistemas</h4>
+						</li>
+						<li>
+							<h2>2010-2013</h2>
+							<h3>Colegio Cervantes</h3>
+							<h4>Bachiller en comunicaciones</h4>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Resume section start -->
+	<section class="resume-section with-bg spad">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-xl-7 offset-xl-2">
+					<div class="section-title">
+						<h2>Cursos</h2>
+					</div>
+					<ul class="resume-list">
+						<li>
+							<h2>2018</h2>
+							<h3>Educacion IT</h3>
+							<h4>Introduccion a base de datos</h4>
+						</li>
+						<li>
+							<h2>2018</h2>
+							<h3>Educacion IT</h3>
+							<h4>Introduccion al paradigma de objetos</h4>
+						</li>
+						<li>
+							<h2>2018</h2>
+							<h3>Educacion IT</h3>
+							<h4>Testing QA</h4>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</section>
+	<!-- Resume section end -->
+
+
+
+	<section class="review-section spad pb-0">
+		<div class="container-fluid">
+			<div class="row">
+				<div class="col-xl-7 offset-xl-2">
+					<div class="section-title">
+						<h2>Skills</h2>
+					</div>
+					<div class="review-slider owl-carousel">
+						<div class="single-review">
+							<div class="qut">“</div>
+							<p>HTML - CSS - JAVASCRIPT - PHP - LARAVEL - ELOQUENT - MySQL - OOP - GIT - METODOLOGÍAS AGILES - SCRUM - PATRON DE ARQUITECTURA MVC - MySQL WORKBENCH - ATOM/VSCODE/CODEBLOCKS - C </p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</section>
+
+
+
+
+
+
+
+
+
+
+	<!--====== Javascripts & Jquery ======-->
+	<script src="js/jquery-2.1.4.min.js"></script>
+	<script src="js/bootstrap.min.js"></script>
+	<script src="js/owl.carousel.min.js"></script>
+	<script src="js/magnific-popup.min.js"></script>
+	<script src="js/circle-progress.min.js"></script>
+	<script src="js/main.js"></script>
+</body>
 </html>
